fc7842fa7f5547d26f689a4a3cdbb611789b79e9
willbrom <alisajid552@yahoo.com>
2018-09-10 17:04:56 +0300

    initial commit

---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+    int max;
+    int min;
+    int guess;
+
+    // Use this for initialization
+    void Start () {
+        StartGame();
+    }
+
+    private void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Welcome to number wizard, yo");
+        Debug.Log("Max number that can be entered is " + max);
+        Debug.Log("Min number that can be entered is " + min);
+        Debug.Log("Initial guess is " + guess);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            Debug.Log("up pressed");
+            min = guess;
+            guess = (max + guess) / 2;
+            Debug.Log("New guess: " + guess);
+        } else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            Debug.Log("down pressed");
+            max = guess;
+            guess = (min + guess) / 2;
+            Debug.Log("New guess: " + guess);
+        } else if (Input.GetKeyDown(KeyCode.Return)) {
+            Debug.Log("Right answer guessed!");
+            StartGame();
+        }
+	}
+}
